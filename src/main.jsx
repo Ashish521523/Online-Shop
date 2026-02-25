80e74d05ccadf44c8e31560f571df89ca91f159d
Ashish Vishwakarma <ashish521523@gmail.com>
2026-02-25 19:20:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./routes/App.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import Home from "./routes/Home.jsx";
 import Bag from "./routes/Bag.jsx";
 import { Provider } from "react-redux";
 import myntraStore from './store/index.js'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
